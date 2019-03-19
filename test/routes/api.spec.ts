0598f81a35a831a5d3da7cf6efdf019f0945c9c8
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-03-19 23:09:40 +0100

    fixed tests

--- test/routes/api.spec.ts
+++ test/routes/api.spec.ts
@@ -23,7 +23,7 @@ describe('baseRoute', () => {
       .request(app)
       .get('/api')
       .then(res => {
-        expect(res.body.title).to.be.equal('Order microservice')
+        expect(res.body.title).to.be.equal('User microservice')
       })
   })
 })
